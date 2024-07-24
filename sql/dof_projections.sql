bd8fc3d2d80e8938f3a2079820cc9dcd45f07293
GregorSchroeder <Gregor.Schroeder@sandag.org>
2024-07-24 14:08:45 -0700

    #43 - update DOF sources

--- sql/dof_projections.sql
+++ sql/dof_projections.sql
@@ -1,26 +1,50 @@
 -- Get the San Diego County population projections from CA DOF
 -- https://dof.ca.gov/forecasting/demographics/projections/
 -- Stratify by year, race, sex, age keeping race categories consistent with ACS
+-- Only interested in base year 2020 data for launch years >= 2020
 -- Temporal resolution is July 1st population
-SELECT [year], [race], [sex], [age], SUM([pop]) AS [pop]
-FROM (
-	SELECT
-		[fiscal_yr] AS [year],
-		CASE	WHEN [race_code] = 1  then 'White alone'
-				WHEN [race_code] = 2  then 'Black or African American alone'
-				WHEN [race_code] = 3  then 'American Indian or Alaska Native alone'
-				WHEN [race_code] = 4  then 'Asian alone'
-				WHEN [race_code] = 5  then 'Native Hawaiian or Other Pacific Islander alone'
-				WHEN [race_code] = 6  then 'Two or More Races'
-				WHEN [race_code] = 7  then 'Hispanic'
-		END AS [race],
-		[sex],
-		[age],
-		[population] AS [pop]
-	FROM
-		[socioec_data].[ca_dof].[population_proj_2021_07_14]
-	-- Vintage 2020 (2021.7.14)
-	WHERE
-        [county_fips_code] = 6073  -- San Diego County
-) AS [tt]
-GROUP BY [year], [race], [sex], [age]
+with
+	[projections]
+	AS
+	(
+		SELECT
+			SUBSTRING(REPLACE([name], 'Vintage ', ''), 1, 4) AS [vintage],
+			MAX([projections_id]) AS [projections_id]
+		-- Take the most recent projection within each vintage
+		FROM [socioec_data].[ca_dof].[projections]
+		GROUP BY SUBSTRING(REPLACE([name], 'Vintage ', ''), 1, 4)
+	),
+	[formatted_data]
+	AS
+	(
+		SELECT
+			[vintage],
+			[year],
+			CASE WHEN [race/ethnicity] = 'White, Non-Hispanic' THEN 'White alone'
+			 WHEN [race/ethnicity] = 'Black, Non-Hispanic' THEN 'Black or African American alone'
+			 WHEN [race/ethnicity] = 'American Indian or Alaska Native, Non-Hispanic' THEN 'American Indian or Alaska Native alone'
+			 WHEN [race/ethnicity] = 'Asian, Non-Hispanic' THEN 'Asian alone'
+			 WHEN [race/ethnicity] = 'Native Hawaiian or Pacific Islander, Non-Hispanic' THEN 'Native Hawaiian or Other Pacific Islander alone'
+			 WHEN [race/ethnicity] = 'Multiracial (two or more of above races), Non-Hispanic' THEN 'Two or More Races'
+			 WHEN [race/ethnicity] = 'Hispanic (any race)' THEN 'Hispanic'
+			 ELSE NULL END AS [race],
+			CASE WHEN [sex] = 'Female' THEN 'F'
+			 WHEN [sex] = 'Male' THEN 'M'
+			 ELSE NULL END AS [sex],
+			CASE WHEN [age] > 110 THEN 110
+             ELSE [age] END AS [age], -- maximum age is 110
+			[population] AS [pop]
+		FROM [socioec_data].[ca_dof].[projections_p3]
+			INNER JOIN [projections]
+			ON [projections_p3].[projections_id] = [projections_p3].[projections_id]
+			INNER JOIN [socioec_data].[ca_dof].[fips]
+			ON [projections_p3].[fips] = [fips].[fips]
+		WHERE
+		[vintage] >= 2020 -- only used for launch years >= 2020
+			AND [year] = 2020 -- only interested in this data for base year 2020
+			AND [fips].[name] = 'San Diego County'
+	)
+SELECT [vintage], [year], [race], [sex], [age], SUM([pop]) AS [pop]
+FROM [formatted_data]
+GROUP BY [vintage], [year], [race], [sex], [age]
+ORDER BY [vintage], [year], [race], [sex], [age]
