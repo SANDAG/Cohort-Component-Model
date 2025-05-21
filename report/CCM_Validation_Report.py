59e1ee040bb45a4f83aaacfaf24cb3001ecdbe94
bryce-sandag <bryce.burdge@sandag.org>
2025-05-21 14:54:26 -0700

    Make comments better

--- report/CCM_Validation_Report.py
+++ report/CCM_Validation_Report.py
@@ -51,9 +51,11 @@ data_selector = st.radio(
 
 # CSV section for what to do if "CSV" option selected
 if data_selector == "CSV":
-    # Check if CSV(S) exist. If not write user error message. If all exist write user message and read in csv files
+    
     # Try to load in CSVs
     csv_dict = report_utils.get_data(data_selector = data_selector, run_id = None)
+    
+    # Save CSVs if they all exist
     if True in csv_dict["population"] and True in csv_dict["components"] and True in csv_dict["rates"]:
         st.session_state.population_data = csv_dict["population"][True]
         st.session_state.components_data = csv_dict["components"][True]
@@ -61,6 +63,7 @@ if data_selector == "CSV":
         # Put messsage saying CSVs loaded successfully, select report from left side menu
         st.success("✅ All CSVs Loaded. Select Report From Left Side Menu")
     
+    # Write error message if one or more CSVs don't load or exist
     else:
         failed = [
             name for name in ["population", "components", "rates"]
@@ -72,12 +75,14 @@ if data_selector == "CSV":
 # Add in section for what to do if "SQL Database" optionn selected
 elif data_selector == "SQL Database":
     
-    # NEED TO LOAD METADATA FIRST AND SELECT A RUN ID
+    # Need to load metadata first and select run_id
     metadata = report_utils.get_metadata()
+    
+    # Logic if metadata exists
     if True in metadata:
 
+        # Save metadata table and display to user to select run_id
         run_df = metadata[True]
-
         runs = st.dataframe(
             data=run_df[["run_id", "user", "date", "version", "comments", "launch", "horizon"]],
             hide_index=True,
@@ -86,8 +91,9 @@ elif data_selector == "SQL Database":
             selection_mode="single-row",
         )
 
-        # What to do if no run_id is selected
+        # Logic if no run_id is selected
         if not runs["selection"]["rows"]:
+            
             # Set session state variables for data to None if haven't selected run
             for key in ["population_dat","components_data", "rates_data"]:
                 if key not in st.session_state:
@@ -97,7 +103,7 @@ elif data_selector == "SQL Database":
             # Display message to user under selection table telling them to select run_id
             st.error(f"Select run_id")
         
-        # What to do once run_id is selected
+        # Logic once run_id is selected
         else:
 
             # Get run_id from user selection and persist selected run_id in session state
@@ -107,7 +113,7 @@ elif data_selector == "SQL Database":
             # Try to load in SQL Data
             sql_dict = report_utils.get_data(data_selector = data_selector, run_id = st.session_state.run_id)
             
-            # Logic if no tables missing
+            # Save tables from SQL Database if they all exist
             if True in sql_dict["population"] and True in sql_dict["components"] and True in sql_dict["rates"]:
 
                     st.session_state.population_data = sql_dict["population"][True]
@@ -116,6 +122,7 @@ elif data_selector == "SQL Database":
                     # Put messsage saying CSVs loaded successfully, select report from left side menu
                     st.success("✅ All CSVs Loaded. Select Report From Left Side Menu")
             
+            # Write error message if one or more SQL tables don't load or exist
             else:
                 failed = [
                     name for name in ["population", "components", "rates"]
@@ -123,9 +130,9 @@ elif data_selector == "SQL Database":
                 ]
                 failed_str = ", ".join(failed)
                 st.error(f"❌ Missing or unreadable SQL Output(s): [{failed_str}]")
-
+    
+    # Write message if can't load Metadata Table
     else: 
-        # Write message if can't load Metadata Table
         st.error(f"❌ Missing or unreadable Metadata table")
 
 # Do nothing if no datasource selected
