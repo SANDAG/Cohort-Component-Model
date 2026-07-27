56d4526db0450ad1ec3569c35bfc635cd7086d2e
KPH <kph@sandag.org>
2026-07-27 14:44:53 -0700

    #154: Update SQL queries

--- sql/mortality/cdc_wonder_mortality.sql
+++ sql/mortality/cdc_wonder_mortality.sql
@@ -1,78 +1,141 @@
-/* This query loads and prepares the CDC WONDER mortality data for the given year by 
-renaming fields to match the CCM fields, removing "Not Stated" rows, and supplementing 
-data to create new race categories for years 2020 and earlier.
+/*
+	This query loads and prepares CDC WONDER mortality data for a given year.
+	The year chosen selects the CDC product used. The 1999-2020 requires the
+	use of "All Races" "Not Hispanic or Latino" as a placeholder for both the
+	"Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+	categories as they do not exist in the product.
 */
 
-DECLARE @year INTEGER;
+DECLARE @year INTEGER = :year;
 
+-- 2021 data is unavailable, fall back to 2020
+IF @year = 2021 AND NOT EXISTS (
+    SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] 
+    WHERE [year] = 2021
+)
 BEGIN
-
--- Add check with THROW statement if the year provided does not exist in the dataset
-IF NOT EXISTS (SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] WHERE [year] = @year)
-    THROW 50002, 'The provided year does not exist in the CDC WONDER mortality dataset', 1
-
+	  SET @year = 2020;
+END
+ELSE
+BEGIN
+    -- For all other years, throw error if year doesn't exist
+    IF NOT EXISTS (
+      SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] 
+      WHERE [year] = @year
+    )
+    THROW 50002, 'The provided year does not exist in the CDC WONDER mortality dataset', 1;
 END;
 
+-- Determine product based on the actual year being used (after fallback)
+DECLARE @product NVARCHAR(9) = CASE
+    WHEN @year >= 2021 THEN '2018+'
+    WHEN @year <=2020 THEN '1999-2020'
+    ELSE NULL END;
 
-WITH [base] AS (
-    SELECT [product]
-          ,[location]
-          ,[period]
-          ,[year]
-          -- Convert age to numeric
-          ,CASE [age]
-          WHEN '85+' THEN '85'
-            ELSE [age]
-          END AS [age]
-          -- Convert sex to single character
-          ,CASE [sex]
-            WHEN 'Female' THEN 'F'
-            WHEN 'Male' THEN 'M'
-          END AS [sex]
-          ,[hispanic_origin]
-          -- Convert races categories
-          ,CASE 
-            WHEN [hispanic_origin] = 'Hispanic or Latino' THEN 'Hispanic'
-            WHEN [race] = 'Asian' THEN 'Asian alone'
-            WHEN [race] = 'Asian or Pacific Islander' THEN 'Asian alone'
-            WHEN [race] = 'Black or African American' THEN 'Black or African American alone'
-            WHEN [race] = 'American Indian or Alaska Native' THEN 'American Indian or Alaska Native alone'
-            WHEN [race] = 'More than one race' THEN 'Two or More Races'
-            WHEN [race] = 'White' THEN 'White alone'
-            WHEN [race] = 'Native Hawaiian or Other Pacific Islander' THEN 'Native Hawaiian or Other Pacific Islander alone'
-            -- Duplicate race categories for 2020 and earlier
-            WHEN [year] <= 2020 AND [mortality].[race] = 'All Races' THEN [splitter].[new_race]
-            ELSE [race]
-          END AS [race]
-          -- Convert 2022+ San Diego County 5-year average deaths to annual deaths
-          -- Population for 2022+ county level is suppressed and will be replaced
-          -- with yearly CCM population estimates which require annual death counts to
-          -- calculate rates
-          ,CASE 
-            WHEN [year] >= 2022 and [location] = 'San Diego County' THEN [deaths] / 5
-            ELSE [deaths] / 1
-          END AS [deaths]
-          ,[pop]
-      FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] AS [mortality]
-      -- Duplicate 'All Races' records into NHPI and MOR for years <= 2020 only
-      -- For years >= 2022, actual race-specific files with sufficient data exist
-      LEFT JOIN (
-          VALUES 
-            ('Native Hawaiian or Other Pacific Islander alone'),
-            ('Two or More Races')
-      ) AS [splitter]([new_race]) ON [mortality].[year] <= 2020 AND [mortality].[race] = 'All Races' AND [mortality].[hispanic_origin] != 'Hispanic or Latino'
-      WHERE [age] NOT IN ('Not Stated', 'All Stated Ages') AND [hispanic_origin] != 'Not Stated' 
-) SELECT 
-    [product]
-    ,[location]
-    ,[period]
-    ,[year]
-    ,[age]
-    ,[sex]
-    ,[hispanic_origin]
-    ,[race]
-    ,[deaths]
-    ,[pop]
-FROM [base]
+WITH [data] AS (
+	SELECT
+		[year]
+		,[location]
+		-- Convert age to numeric
+		,CASE
+			WHEN [age] = '85+' THEN '85'
+			ELSE [age]
+		END AS [age]
+		-- Convert sex to single character
+		,CASE [sex]
+			WHEN 'Female' THEN 'F'
+			WHEN 'Male' THEN 'M'
+		END AS [sex]
+		-- Convert race categories to consistent definition across products
+		,CASE 
+			WHEN [hispanic_origin] = 'Hispanic or Latino' THEN 'Hispanic'
+			WHEN [race] = 'Asian' THEN 'Asian alone'
+			WHEN [race] = 'Asian or Pacific Islander' THEN 'Asian alone'
+			WHEN [race] = 'Black or African American' THEN 'Black or African American alone'
+			WHEN [race] = 'American Indian or Alaska Native' THEN 'American Indian or Alaska Native alone'
+			WHEN [race] = 'More than one race' THEN 'Two or More Races'
+			WHEN [race] = 'White' THEN 'White alone'
+			WHEN [race] = 'Native Hawaiian or Other Pacific Islander' THEN 'Native Hawaiian or Other Pacific Islander alone'
+			-- "All Races" values are used only within the 1999-2020 product as a placeholder
+			-- For "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+			WHEN @product = '1999-2020' AND [race] = 'All Races' THEN 'All Races'
+			ELSE [race]
+		END AS [race]
+		,CASE
+			-- Population is suppressed at the county level in the 2018+ CDC WONDER product
+			-- We are forced to use the population within the CCM software which is not a five year sum
+			WHEN @product = '2018+' AND [location] = 'San Diego County' THEN [deaths]/5.0
+			ELSE [deaths]/1.0
+		END AS [deaths]
+		,[pop]
+	FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality]
+	WHERE
+		[product] = @product
+		-- Five year rolling sums used
+		AND [period] = 'Five-Year'
+		-- These values are only used in the inflation factor calculation
+		AND [age] NOT IN (
+			'All Stated Ages',
+			'Not Stated'
+		)
+		-- These values are only used in the inflation factor calculation
+		AND [hispanic_origin] NOT IN (
+			'Hispanic or Latino; Not Hispanic or Latino',
+			'All Origins',
+			'Not Stated'
+		)
+		-- "All Races" values are used only within the 1999-2020 product as a placeholder
+		-- For "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+		AND NOT (
+			[product] = '2018+'
+			AND [race] = 'All Races'
+			AND [hispanic_origin] = 'Not Hispanic or Latino'
+		)
+)
+SELECT
+	[year]
+	,[location]
+	,[age]
+	,[sex]
+	,[race]
+	,[deaths]
+	,[pop]
+FROM [data]
 WHERE [race] != 'All Races' AND [year] = @year
-ORDER BY [product], [location], [year], [age], [sex], [hispanic_origin], [race] 
+
+-- Following UNION statements use the "All Races" "Not Hispanic or Latino"
+-- To create "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+UNION ALL
+
+SELECT
+	[year]
+	,[location]
+	,[age]
+	,[sex]
+	,'Two or More Races' AS [race]
+	,[deaths]
+	,[pop]
+FROM [data]
+WHERE [race] = 'All Races' AND [year] = @year
+
+
+UNION ALL
+
+SELECT
+	[year]
+	,[location]
+	,[age]
+	,[sex]
+	,'Native Hawaiian or Other Pacific Islander alone' AS [race]
+	,[deaths]
+	,[pop]
+FROM [data]
+WHERE [race] = 'All Races' AND [year] = @year
+
+
+ORDER BY
+	[year]
+	,[location]
+	,[age]
+	,[sex]
+	,[race]
--- sql/mortality/cdc_wonder_mortality_inflation.sql
+++ sql/mortality/cdc_wonder_mortality_inflation.sql
@@ -1,53 +1,60 @@
 /* This query calculates the CDC WONDER mortality inflation factors that will be used to
 inflate deaths. The inflation factor is calculated as 1 + (Not Stated Deaths / 
-Stated Deaths) for each location and sex. 
+Total Deaths) for each year, location, and sex. 
 */
 
-WITH [base] AS (
-    SELECT [product]
-          ,[location]
-          ,[period]
-          ,[year]
-          ,[age]
-          -- Convert sex to single character
-          ,CASE [sex]
-            WHEN 'Female' THEN 'F'
-            WHEN 'Male' THEN 'M'
-          END AS [sex]
-          ,[hispanic_origin]
-          -- Convert 2022+ San Diego County 5-year average deaths to annual deaths
-          -- Population for 2022+ county level is suppressed and will be replaced
-          -- with yearly CCM population estimates which require annual death counts to
-          -- calculate rates
-          ,CASE 
-            WHEN [year] >= 2022 and [location] = 'San Diego County' THEN [deaths] / 5
-            ELSE [deaths] / 1
-          END AS [deaths]
-          ,[pop]
-      FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality]
--- Create Not Stated deaths
-), [not_stated] AS (
-    SELECT 
-        [location]
-        ,[sex]
-        ,SUM([deaths]) AS [deaths] 
-    FROM [base]
-    WHERE [hispanic_origin] = 'Not Stated' OR [age] = 'Not Stated'
-    GROUP BY [location], [sex]
--- Create Stated deaths
-), [stated] AS (
-    SELECT 
-        [location]
-        ,[sex]
-        ,SUM([deaths]) AS [deaths] 
-    FROM [base]
-    WHERE [age] = 'All Stated Ages'
-    GROUP BY [location], [sex]
--- Create inflation factor
-) SELECT 
-    [not_stated].[location]
-    ,[not_stated].[sex]
-    ,1 + ([not_stated].[deaths] / [stated].[deaths]) AS inflation_factor
-FROM 
-(SELECT * FROM [not_stated]) AS [not_stated]
-LEFT JOIN (SELECT * FROM [stated]) AS [stated] ON [stated].[location] = [not_stated].[location] AND [stated].[sex] = [not_stated].[sex]
+DECLARE @year INTEGER = :year;
+
+-- 2021 data is unavailable, fall back to 2020
+IF @year = 2021 AND NOT EXISTS (
+    SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] 
+    WHERE [year] = 2021
+)
+BEGIN
+	  SET @year = 2020;
+END
+ELSE
+BEGIN
+    -- For all other years, throw error if year doesn't exist
+    IF NOT EXISTS (
+      SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality] 
+      WHERE [year] = @year
+    )
+    THROW 50002, 'The provided year does not exist in the CDC WONDER mortality dataset', 1;
+END;
+
+SELECT
+    [year],
+    [location],
+    CASE [sex]
+        WHEN 'Female' THEN 'F'
+        WHEN 'Male' THEN 'M'
+    END AS [sex],
+    1 + SUM(
+        CASE
+            WHEN [age] = 'Not Stated' OR [hispanic_origin] = 'Not Stated' THEN [deaths]
+            ELSE 0
+        END) / SUM(
+        CASE 
+            WHEN [year] >= 2022 AND [location] = 'San Diego County' THEN [deaths] / 5.0
+            ELSE [deaths]
+        END) * 1.0 AS [inflation_factor]
+FROM [socioec_data].[vital_statistics].[cdc_wonder_mortality]
+WHERE
+    -- We load "All Stated Ages" specifically to capture "Not Stated" records in Hispanic Origin
+    -- As we do not want to load in single year of age data for "Not Stated" Hispanic Origin records
+    [age] IN ('All Stated Ages', 'Not Stated')
+    -- We load "Hispanic or Latino; Not Hispanic or Latino" to combine with "All Stated Ages"
+    -- To make the calculation of total deaths for stated categories easier
+    -- We load "All Origins" to combine with "Not Stated" age
+    AND [hispanic_origin] IN ('Hispanic or Latino; Not Hispanic or Latino', 'All Origins', 'Not Stated')
+    AND [race] = 'All Races'
+    AND [year] = @year
+GROUP BY
+    [year],
+    [location],
+    [sex]
+ORDER BY
+    [year],
+    [location],
+    [sex]
