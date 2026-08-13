89a3540d6097a177fb9353d7dace691d93d848f2
KPH <kph@sandag.org>
2026-08-13 11:53:51 -0700

    #161: Update sql fertility script

--- sql/fertility/cdc_wonder_fertility.sql
+++ sql/fertility/cdc_wonder_fertility.sql
@@ -5,102 +5,113 @@
 */
 
 DECLARE @year INTEGER = :year;
+DECLARE @msg nvarchar(49) = 'Data for CDC WONDER fertility year does not exist';
+DECLARE @product NVARCHAR(9) = CASE
+    WHEN @year >= 2020 THEN '2020+'
+    WHEN @year <= 2019 THEN '2007-2019'
+    ELSE NULL END;
 
+-- Send error message if no data exists --------------------------------------
+IF NOT EXISTS (
+    SELECT TOP (1) *
+    FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility]
+    WHERE 
+        [year] = @year
+)
+SELECT @msg AS [msg]
+ELSE
 BEGIN
-    -- Add check with THROW statement if the year provided does not exist in the dataset
-    IF NOT EXISTS (SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility] WHERE [year] = @year)
-        THROW 50002, 'The provided year does not exist in the CDC WONDER fertility dataset', 1
+
+    WITH [data] AS (
+        SELECT [product]
+            ,[location]
+            ,[period]
+            ,[year]
+            ,[age]
+            ,[hispanic_origin]
+            ,CASE 
+                WHEN [hispanic_origin] = 'Hispanic or Latino' THEN 'Hispanic'
+                WHEN [race] = 'Asian' THEN 'Asian alone'
+                WHEN [race] = 'Asian or Pacific Islander' THEN 'Asian alone'
+                WHEN [race] = 'Black or African American' THEN 'Black or African American alone'
+                WHEN [race] = 'American Indian or Alaska Native' THEN 'American Indian or Alaska Native alone'
+                WHEN [race] = 'More than one race' THEN 'Two or More Races'
+                WHEN [race] = 'White' THEN 'White alone'
+                WHEN [race] = 'Native Hawaiian or Other Pacific Islander' THEN 'Native Hawaiian or Other Pacific Islander alone'
+                -- "All Races" values are used only before 2020 as a placeholder
+                -- For "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+                WHEN [product] = '2007-2019' AND [race] = 'All Races' THEN 'All Races'
+                ELSE [race]
+            END AS [race]
+            -- Divide by five to get annual average births
+            ,[rate] / 1000.0 AS [rate]
+        FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility] AS [fertility]
+        -- For years >= 2022, actual race-specific files with sufficient data exist
+        WHERE 
+            [age] NOT IN ('Under 15 years', '50 years and over') 
+            AND [hispanic_origin] != 'Not Stated' 
+            AND [race] != 'Not Stated'
+            AND [year] = @year
+    ),
+    [race_expanded] AS (
+        SELECT 
+            [location]
+            ,[year]
+            ,[age]
+            ,[hispanic_origin]
+            ,[race]
+            ,[rate]
+        FROM [data]
+        -- Following UNION statements use the "All Races"
+        -- To create "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
+        UNION ALL
+
+        SELECT
+            [location]
+            ,[year]
+            ,[age]
+            ,[hispanic_origin]
+            ,'Two or More Races' AS [race]
+            ,[rate]
+        FROM [data]
+        WHERE [race] = 'All Races' AND [year] = @year AND [year] <= 2019
+
+        UNION ALL
+
+        SELECT
+            [location]
+            ,[year]
+            ,[age]
+            ,[hispanic_origin]
+            ,'Native Hawaiian or Other Pacific Islander alone' AS [race]
+            ,[rate]
+        FROM [data]
+        WHERE [race] = 'All Races' AND [year] = @year AND [year] <= 2019
+    )   SELECT 
+            [location]
+            ,[year]
+            ,[single_age].[age_group]
+            ,[single_age].[age]
+            ,[hispanic_origin]
+            ,[race]
+            ,[rate]
+        FROM [race_expanded]
+        CROSS JOIN (
+            VALUES 
+                ('15-19 years', 15), ('15-19 years', 16), ('15-19 years', 17), ('15-19 years', 18), ('15-19 years', 19),
+                ('20-24 years', 20), ('20-24 years', 21), ('20-24 years', 22), ('20-24 years', 23), ('20-24 years', 24),
+                ('25-29 years', 25), ('25-29 years', 26), ('25-29 years', 27), ('25-29 years', 28), ('25-29 years', 29),
+                ('30-34 years', 30), ('30-34 years', 31), ('30-34 years', 32), ('30-34 years', 33), ('30-34 years', 34),
+                ('35-39 years', 35), ('35-39 years', 36), ('35-39 years', 37), ('35-39 years', 38), ('35-39 years', 39),
+                ('40-44 years', 40), ('40-44 years', 41), ('40-44 years', 42), ('40-44 years', 43), ('40-44 years', 44)
+        ) AS [single_age]([age_group], [age])
+        WHERE 
+            [race_expanded].[age] = [single_age].[age_group]
+            AND [race_expanded].[race] != 'All Races'
+        ORDER BY 
+            [location] 
+            ,[year]
+            ,[single_age].[age] 
+            ,[hispanic_origin]
+            ,[race] 
 END;
-
-WITH [data] AS (
-    SELECT [product]
-        ,[location]
-        ,[period]
-        ,[year]
-        ,[age]
-        ,[hispanic_origin]
-        ,CASE 
-            WHEN [hispanic_origin] = 'Hispanic or Latino' THEN 'Hispanic'
-            WHEN [race] = 'Asian' THEN 'Asian alone'
-            WHEN [race] = 'Asian or Pacific Islander' THEN 'Asian alone'
-            WHEN [race] = 'Black or African American' THEN 'Black or African American alone'
-            WHEN [race] = 'American Indian or Alaska Native' THEN 'American Indian or Alaska Native alone'
-            WHEN [race] = 'More than one race' THEN 'Two or More Races'
-            WHEN [race] = 'White' THEN 'White alone'
-            WHEN [race] = 'Native Hawaiian or Other Pacific Islander' THEN 'Native Hawaiian or Other Pacific Islander alone'
-   			-- "All Races" values are used only before 2020 as a placeholder
-			-- For "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
-			WHEN [year] <= 2019 AND [race] = 'All Races' THEN 'All Races'
-            ELSE [race]
-        END AS [race]
-        -- Divide by five to get annual average births
-        ,[births] / 5.0 AS [births]
-    FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility] AS [fertility]
-    -- For years >= 2022, actual race-specific files with sufficient data exist
-    WHERE 
-        [age] NOT IN ('Under 15 years', '50 years and over') 
-        AND [hispanic_origin] != 'Not Stated' 
-        AND [race] != 'Not Stated'
-        AND [year] = @year
-),
-[race_expanded] AS (
-    SELECT 
-        [location]
-        ,[year]
-        ,[age]
-        ,[hispanic_origin]
-        ,[race]
-        ,[births]
-    FROM [data]
-    -- Following UNION statements use the "All Races"
-    -- To create "Two or More Races" and "Native Hawaiian or Other Pacific Islander alone"
-    UNION ALL
-
-    SELECT
-        [location]
-        ,[year]
-        ,[age]
-        ,[hispanic_origin]
-        ,'Two or More Races' AS [race]
-        ,[births]
-    FROM [data]
-    WHERE [race] = 'All Races' AND [year] = @year AND [year] <= 2019
-
-    UNION ALL
-
-    SELECT
-        [location]
-        ,[year]
-        ,[age]
-        ,[hispanic_origin]
-        ,'Native Hawaiian or Other Pacific Islander alone' AS [race]
-        ,[births]
-    FROM [data]
-    WHERE [race] = 'All Races' AND [year] = @year AND [year] <= 2019
-)   SELECT 
-        [location]
-        ,[year]
-        ,[single_age].[age_group]
-        ,[single_age].[age]
-        ,[hispanic_origin]
-        ,[race]
-        ,[births]
-    FROM [race_expanded]
-    CROSS JOIN (
-        VALUES 
-            ('15-19 years', 15), ('15-19 years', 16), ('15-19 years', 17), ('15-19 years', 18), ('15-19 years', 19),
-            ('20-24 years', 20), ('20-24 years', 21), ('20-24 years', 22), ('20-24 years', 23), ('20-24 years', 24),
-            ('25-29 years', 25), ('25-29 years', 26), ('25-29 years', 27), ('25-29 years', 28), ('25-29 years', 29),
-            ('30-34 years', 30), ('30-34 years', 31), ('30-34 years', 32), ('30-34 years', 33), ('30-34 years', 34),
-            ('35-39 years', 35), ('35-39 years', 36), ('35-39 years', 37), ('35-39 years', 38), ('35-39 years', 39),
-            ('40-44 years', 40), ('40-44 years', 41), ('40-44 years', 42), ('40-44 years', 43), ('40-44 years', 44)
-    ) AS [single_age]([age_group], [age])
-    WHERE 
-        [race_expanded].[age] = [single_age].[age_group]
-        AND [race_expanded].[race] != 'All Races'
-    ORDER BY 
-        [location] 
-        ,[year]
-        ,[single_age].[age] 
-        ,[hispanic_origin]
-        ,[race] 
--- sql/fertility/cdc_wonder_fertility_inflation.sql
+++ sql/fertility/cdc_wonder_fertility_inflation.sql
@@ -5,33 +5,44 @@
 */
 
 DECLARE @year INTEGER = :year;
+DECLARE @msg nvarchar(49) = 'Data for CDC WONDER fertility year does not exist';
+DECLARE @product NVARCHAR(9) = CASE
+    WHEN @year >= 2020 THEN '2020+'
+    WHEN @year <= 2019 THEN '2007-2019'
+    ELSE NULL END;
 
+-- Send error message if no data exists --------------------------------------
+IF NOT EXISTS (
+    SELECT TOP (1) *
+    FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility]
+    WHERE 
+        [year] = @year
+)
+SELECT @msg AS [msg]
+ELSE
 BEGIN
-    -- Add check with THROW statement if the year provided does not exist in the dataset
-    IF NOT EXISTS (SELECT 1 FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility] WHERE [year] = @year)
-        THROW 50002, 'The provided year does not exist in the CDC WONDER fertility dataset', 1
-END;
 
-SELECT
-    [year],
-    [location],
-    1 + SUM(
-        CASE
-            WHEN [race] = 'Not Stated' OR [hispanic_origin] = 'Not Stated' THEN [births]
-            ELSE 0
-        END) / SUM([births]) * 1.0 AS [inflation_factor]
-FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility]
-WHERE
-    -- We load "Hispanic or Latino; Not Hispanic or Latino" to combine with "Not Stated" Race
-    -- And "Not Stated" Hispanic Origin to combine with "All Races" to account for "Not Stated" Records
-    -- We load "Hispanic or Latino" and "Not Hispanic or Latino"
-    -- To make the calculation of total births 
-    [hispanic_origin] IN ('Hispanic or Latino', 'Not Hispanic or Latino', 'Not Stated', 'Hispanic or Latino; Not Hispanic or Latino')
-    AND [race] IN ('All Races', 'Not Stated') AND [age] NOT IN ('Under 15 years', '50 years and over')
-    AND [year] = @year
-GROUP BY
-    [year],
-    [location]
-ORDER BY
-    [year],
-    [location]
+    SELECT
+        [year],
+        [location],
+        1 + SUM(
+            CASE
+                WHEN [race] = 'Not Stated' OR [hispanic_origin] = 'Not Stated' OR [age] IN ('Under 15 years' ,'45-49 years', '50 years and over') THEN [births]
+                ELSE 0
+            END) / SUM([births]) * 1.0 AS [inflation_factor]
+    FROM [socioec_data].[vital_statistics].[cdc_wonder_fertility]
+    WHERE
+        -- We load "Hispanic or Latino; Not Hispanic or Latino" to combine with "Not Stated" Race
+        -- And "Not Stated" Hispanic Origin to combine with "All Races" to account for "Not Stated" Records
+        -- We load "Hispanic or Latino" and "Not Hispanic or Latino"
+        -- To make the calculation of total births 
+        [hispanic_origin] IN ('Hispanic or Latino', 'Not Hispanic or Latino', 'Not Stated', 'Hispanic or Latino; Not Hispanic or Latino')
+        AND [race] IN ('All Races', 'Not Stated')
+        AND [year] = @year
+    GROUP BY
+        [year],
+        [location]
+    ORDER BY
+        [year],
+        [location]
+END;
